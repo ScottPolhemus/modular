036dfbdf43cd46800b4901a7ae742c3143efb162
ScottPolhemus <scott.polhemus@barrelny.com>
2015-11-12 00:26:29 -0500

    Document carousel script

--- modules/carousel/carousel.js
+++ modules/carousel/carousel.js
@@ -1,5 +1,10 @@
 var Flickity = require('flickity');
 
+/**
+ * Initialize a carousel module with Flickity.
+ * @constructor
+ * @param {Object} element - The DOM element for this instance
+ */
 function Carousel(element) {
   this.el = element;
   this.flickity = new Flickity(this.el);
